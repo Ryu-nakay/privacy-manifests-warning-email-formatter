91cb76f24a2f4a36209bc0299554cb6ce94a32da
Ryu-nakay <ryukenkana@gmail.com>
2024-03-16 18:55:39 +0900

    first commit

---
+++ PrivacyManifestsWarningEmailFormatter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrivacyManifestsWarningEmailFormatter
+//
+//  Created by 中山龍 on 2024/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrivacyManifestsWarningEmailFormatter/PrivacyManifestsWarningEmailFormatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyManifestsWarningEmailFormatterApp.swift
+//  PrivacyManifestsWarningEmailFormatter
+//
+//  Created by 中山龍 on 2024/03/16.
+//
+
+import SwiftUI
+
+@main
+struct PrivacyManifestsWarningEmailFormatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
